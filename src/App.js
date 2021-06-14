a9f00d439c9798b972bd189cefb26a25be525500
João Pedro <joaof082@gmail.com>
2021-06-14 01:25:27 -0300

    Iniciando a pagina de login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Button from './components/Button /Button'
+
+import Login from "./pages/Login/Login"
 
 export default function App() {
   return (
     <div>
-      <>Clone do instagram</>
+      <Button />
+      <Login />
     </div>
   )
 }
